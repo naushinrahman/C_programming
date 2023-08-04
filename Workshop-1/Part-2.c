95bca8899ed6a0d5881e5309e614678f35ce08c7
naushinrahman <130614154+naushinrahman@users.noreply.github.com>
2023-08-04 00:24:45 -0400

    Add file

---
+++ Workshop-1/Part-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+int main() {
+	printf("Workshop 1 Part-2\n");
+	printf("=================\n\n");
+	printf("Using tab specifiers...\n");
+	printf("\tTab-1\tTab-2\tTab-3\n");
+	printf("\t-----\t=====\t-----\n\n");
+	printf("\\ this is a back-slash character!\n");
+	printf("%% this a percent sign character!\n");
+	printf("\" this is a double - quote character!\n\n");
+	printf("My favourite quotes are:\n");
+	printf("1.\t\"Skill is only developed by hours and hours of work.\"\n");
+	printf("\t\t\t\t\t\t-Usain Bolt\n\n");
+	printf("2.\t\"It's not about having time. It's about making time.\"\n");
+	printf("\t\t\t\t\t\t-unknown\n\n");
+	printf("3.\t\"All of us do not have equal talent. But, all of us\n");
+	printf("\thave an equal opportunity to develop our talents.\"\n");
+	printf("\t\t\t\t\t\t-A.P.J. Abdul Kalem");
+return 0;
+}
